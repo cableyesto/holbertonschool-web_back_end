240061d1d44cdbe8f7d498c61ae208866f1113c2
cableyesto <dialkins@tutamail.com>
2025-10-20 13:11:58 +0200

    Done task 1. Log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -2,6 +2,7 @@
 """ Filtered Logger Script """
 
 import re
+import logging
 
 
 def filter_datum(fields, redaction, message, separator):
@@ -11,3 +12,22 @@ def filter_datum(fields, redaction, message, separator):
         regex = str(field) + r'=\s*([^' + str(separator) + r']*)'
         res = re.sub(regex, str(field) + '=' + redaction, res)
     return res
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        res = filter_datum(self.fields, '***', re.sub(r';', '; ',
+                           record.getMessage()), ';')
+        log = logging.LogRecord("my_logger", logging.INFO, None, None,
+                                res, None, None)
+        return super().format(log)
