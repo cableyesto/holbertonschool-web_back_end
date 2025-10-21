c519730787a906cceb2b59d53cfdffb067407d23
cableyesto <dialkins@tutamail.com>
2025-10-21 17:03:30 +0200

    Done task 4. Define which routes don't need authentication.

--- Basic_authentication/api/v1/auth/auth.py
+++ Basic_authentication/api/v1/auth/auth.py
@@ -9,7 +9,14 @@ class Auth:
     """ Class Authentication """
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Require authentication """
-        return False
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path in excluded_paths or path + '/' in excluded_paths:
+            return False
+        else:
+            return True
 
     def authorization_header(self, request=None) -> str:
         """ Authorization header """
