120f635f2a6462417e8dbbbddf6518ea8ce2505d
cableyesto <dialkins@tutamail.com>
2025-10-21 16:44:58 +0200

    Task done 2. Error handler: Forbidden.

--- Basic_authentication/api/v1/views/index.py
+++ Basic_authentication/api/v1/views/index.py
@@ -33,3 +33,12 @@ def status_unauthorized() -> str:
       - the unauthorized route
     """
     abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def status_forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return:
+      - the forbidden route
+    """
+    abort(403)
