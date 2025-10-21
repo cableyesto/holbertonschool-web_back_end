2a6e36e15f3f5716198917a644ff045656ca7ffc
cableyesto <dialkins@tutamail.com>
2025-10-21 11:52:53 +0200

    Add annotation for task 3.

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -62,7 +62,7 @@ def get_logger() -> logging.Logger:
     return logger
 
 
-def get_db():
+def get_db() -> mysql.connector.connection.MySQLConnection:
     """ Return a db connection """
     db_username = os.environ["PERSONAL_DATA_DB_USERNAME"]
     db_pass = os.environ["PERSONAL_DATA_DB_PASSWORD"]
