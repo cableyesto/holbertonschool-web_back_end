7930e0758dc0ee2b696712a3b5055a5f86fd0874
cableyesto <dialkins@tutamail.com>
2025-10-22 14:59:55 +0200

    Done task 8. Basic - Base64 decode.

--- Basic_authentication/api/v1/auth/basic_auth.py
+++ Basic_authentication/api/v1/auth/basic_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 from re import search
+from base64 import b64encode, b64decode
 
 
 class BasicAuth(Auth):
@@ -25,3 +26,25 @@ class BasicAuth(Auth):
             regex_after = r'(?<=\bBasic\s).*'
             res_match = search(regex_after, authorization_header)
             return res_match[0]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header: str
+                                           ) -> str:
+        """ Decode base64 header """
+        if base64_authorization_header is None:
+            return None
+
+        if isinstance(base64_authorization_header, str) is False:
+            return None
+
+        try:
+            decoded = b64decode(base64_authorization_header,
+                                validate=True)
+            encoded = b64encode(decoded).decode("utf-8")
+            res = encoded == base64_authorization_header
+            if res is False:
+                return None
+            else:
+                return decoded.decode("utf-8")
+        except Exception:
+            return None
