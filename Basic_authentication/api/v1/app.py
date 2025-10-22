c9d3beb752ee816851d0865f43ae45896c1490ab
cableyesto <dialkins@tutamail.com>
2025-10-22 10:05:43 +0200

    Try to correct error Variable auth is an instance of BasicAuth.

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -62,7 +62,7 @@ if __name__ == "__main__":
     if auth == "auth":
         from api.v1.auth.auth import Auth
         auth = Auth()
-    elif auth == "basic_auth":
+    if auth == "basic_auth":
         from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
 
