545f517fe80453830a1938c8ec6124fe51748b6b
cableyesto <dialkins@tutamail.com>
2025-10-22 10:48:11 +0200

    Change top level declartion of modules.

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -7,14 +7,15 @@ from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
 from flask_cors import (CORS, cross_origin)
 import os
+from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
 
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-# auth = None
-auth = BasicAuth()
+auth = None
+# auth = BasicAuth()
 
 
 @app.errorhandler(404)
@@ -62,10 +63,10 @@ if __name__ == "__main__":
     port = getenv("API_PORT", "5000")
     auth_env = getenv("AUTH_TYPE", "auth")
     if auth_env == "auth":
-        from api.v1.auth.auth import Auth
+        # from api.v1.auth.auth import Auth
         auth = Auth()
     elif auth_env == "basic_auth":
-        from api.v1.auth.basic_auth import BasicAuth
+        # from api.v1.auth.basic_auth import BasicAuth
         auth = BasicAuth()
 
     app.run(host=host, port=port)
