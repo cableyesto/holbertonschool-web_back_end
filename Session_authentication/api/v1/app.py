4fb236af9da82c5ac1a054bd4f31af7886fcf0f2
cableyesto <dialkins@tutamail.com>
2025-11-12 16:26:11 +0100

    Done task 1. Empty session.

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -9,6 +9,7 @@ from flask_cors import (CORS, cross_origin)
 import os
 from api.v1.auth.auth import Auth
 from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -67,5 +68,7 @@ if __name__ == "__main__":
         auth = Auth()
     elif auth_env == "basic_auth":
         auth = BasicAuth()
+    elif auth_env == "session_auth":
+        auth = SessionAuth()
 
     app.run(host=host, port=port)
