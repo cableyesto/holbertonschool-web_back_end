fe3e28ba8b147a00796b5b4890c67db0ef625398
cableyesto <dialkins@tutamail.com>
2025-11-17 15:07:07 +0100

    Done task 2. Find user.

--- user_authentication_service/db.py
+++ user_authentication_service/db.py
@@ -5,6 +5,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.session import Session
+from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 
 from user import Base, User
 
@@ -37,3 +39,26 @@ class DB:
         session.add(user)
         session.commit()
         return user
+
+    def find_user_by(self, **kwargs) -> User:
+        """ Find a user """
+        user_keys = []
+        for column in User.__table__.columns.keys():
+            user_keys.append(column)
+
+        invalid = set(kwargs.keys()) - set(user_keys)
+        if invalid:
+            raise InvalidRequestError
+
+        session = self._session
+        arg = list(kwargs.keys())
+        first_arg = arg[0]
+        user_found = (
+            session.query(User)
+            .filter_by(**{first_arg: kwargs[first_arg]})
+            .first()
+        )
+        if not user_found:
+            raise NoResultFound
+
+        return user_found
