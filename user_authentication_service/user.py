9a6aace5c0e4ef6fb30b53fefe2e052d27297713
cableyesto <dialkins@tutamail.com>
2025-11-17 11:01:04 +0100

    Respect requirements.

--- user_authentication_service/user.py
+++ user_authentication_service/user.py
@@ -12,6 +12,7 @@ Base = declarative_base()
 
 
 class User(Base):
+    """ User Model Class """
     __tablename__ = 'users'
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
