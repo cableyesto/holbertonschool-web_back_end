40803faeb8e1528823505929ca5ef1f082995bf0
cableyesto <dialkins@tutamail.com>
2025-11-18 17:10:34 +0100

    Done task 10. Get session ID.

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -52,3 +52,14 @@ class Auth:
 
         except NoResultFound:
             return False
+
+    def create_session(self, email: str) -> str:
+        """ Create a session ID """
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = _generate_uuid()
+            self._db.update_user(user.id, session_id=uuid)
+            return user.session_id
+        
+        except NoResultFound:
+            return None
