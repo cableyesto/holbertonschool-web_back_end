17c59663d52b17a7c223ce8e82a74961ba3cf12d
cableyesto <dialkins@tutamail.com>
2025-11-18 17:31:47 +0100

    Done task  11. Log in.

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,7 +2,7 @@
 """
 App module
 """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, abort
 from auth import Auth
 from user import User
 
@@ -26,6 +26,22 @@ def users() -> str:
         return jsonify({"email": user.email, "message": "user created"}), 200
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
+
+
+@app.route("/sessions", methods=["POST"])
+def login() -> str:
+    """ Login route """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    is_valid = AUTH.valid_login(email, password)
+    if not is_valid:
+        abort(401)
+
+    uuid = AUTH.create_session(email)
+
+    res = jsonify({"email": email, "message": "logged in"})
+    res.set_cookie("session_id", uuid)
+    return res, 200
 
 
 if __name__ == "__main__":
