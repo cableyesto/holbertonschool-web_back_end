679d89640f64887bd8ffd096c296db002cfcdf4c
cableyesto <dialkins@tutamail.com>
2025-11-18 16:44:27 +0100

    Done task  7. Register user.

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -2,14 +2,30 @@
 """
 App module
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+from user import User
 
 app = Flask(__name__)
+AUTH = Auth()
 
 
 @app.route("/", methods=["GET"])
-def welcome():
+def welcome() -> str:
+    """ Welcome route """
     return jsonify({"message": "Bienvenue"}), 200
+
+
+@app.route("/users", methods=["POST"])
+def users() -> str:
+    """ Users creation route """
+    try:
+        email = request.form.get("email")
+        password = request.form.get("password")
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": user.email, "message": "user created"}), 200
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == "__main__":
