1714b1e4d07963c970790fcf01af8696a08c65dd
cableyesto <dialkins@tutamail.com>
2025-11-19 10:13:51 +0100

    Done task 16. Generate reset password token.

--- user_authentication_service/auth.py
+++ user_authentication_service/auth.py
@@ -84,3 +84,15 @@ class Auth:
 
         except NoResultFound:
             return None
+
+    def get_reset_password_token(self, email: str) -> str:
+        """ Send reset password token """
+        try:
+            user = self._db.find_user_by(email=email)
+            uuid = _generate_uuid()
+            self._db.update_user(user.id, reset_token=uuid)
+
+            return uuid
+
+        except NoResultFound:
+            raise ValueError
