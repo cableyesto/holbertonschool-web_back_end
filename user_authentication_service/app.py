b799c40449a741a2fb0b25be28d611b1a923c3fa
cableyesto <dialkins@tutamail.com>
2025-11-19 10:46:54 +0100

    Done task  17. Get reset password token.

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -80,5 +80,16 @@ def profile():
     return jsonify({"email": user.email}), 200
 
 
+@app.route("/reset_password", methods=["POST"])
+def get_reset_password_token():
+    """ Reset password route """
+    email = request.form.get("email")
+    try:
+        uuid = AUTH.get_reset_password_token(email)
+    except ValueError:
+        abort(403)
+    return jsonify({"email": email, "reset_token": uuid}), 200
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
