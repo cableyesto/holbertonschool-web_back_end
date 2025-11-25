08d759f74b8c69a1e46c043e0c340b180dacc205
cableyesto <dialkins@tutamail.com>
2025-11-25 10:56:42 +0100

    Initialize repository Unittests and Integration Tests. Done task 0. Parameterize a unit test.

---
+++ Unittests_and_integration_tests/test_utils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+""" Unit tests and integration tests module"""
+
+import unittest
+from parameterized import parameterized
+from utils import access_nested_map
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
+
+
+class TestAccessNestedMap(unittest.TestCase):
+    """ Unit test the access_nested_map """
+    @parameterized.expand([
+        ({"a": 1}, ("a",), 1),
+        ({"a": {"b": 2}}, ("a",), {"b": 2}),
+        ({"a": {"b": 2}}, ("a", "b"), 2)
+    ])
+    def test_access_nested_map(self,
+                               nested_map: Mapping,
+                               path: Sequence,
+                               expected: Any) -> bool:
+        """ Testing the method to return the correct path """
+        self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == '__main__':
+    unittest.main()
