7679933256b1426d6e8cd3e5833e05438e71ab62
cableyesto <dialkins@tutamail.com>
2025-11-26 10:40:54 +0100

    Done task  1. Basic Babel setup.

---
+++ i18n/1-app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""
+App module
+"""
+from flask import Flask, request, render_template
+from flask_babel import Babel
+from pytz import timezone
+
+
+class Config:
+    """ Configuration class of the flask app """
+    LANGUAGES = ["en", "fr"]
+    TZ = timezone("UTC")
+
+
+# def get_locale():
+#     """ Get the local from the Config class """
+#     return request.accept_languages.best_match(Config.LANGUAGES)
+
+
+app = Flask(__name__)
+babel = Babel(app, locale_selector=Config.LANGUAGES[0], timezone_selector=Config.TZ)
+
+@app.route("/", methods=["GET"])
+def index() -> str:
+    """ Index render route """
+    return render_template('1-index.html')
+
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port="5000")
