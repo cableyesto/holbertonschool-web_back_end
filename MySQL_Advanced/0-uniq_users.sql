b201639305c5f1ccc7053c9ddbf938e4472326f1
cableyesto <dialkins@tutamail.com>
2025-12-01 11:01:47 +0100

    Initialize MySQL advanced. Done task 0. We are all unique!

---
+++ MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- 0. We are all unique!
+-- Create a table users
+CREATE TABLE IF NOT EXISTS users (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(255) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
