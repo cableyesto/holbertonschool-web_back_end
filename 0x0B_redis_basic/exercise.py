ba50b6c52bae8bc7a114ec14580106a0dd252d45
cableyesto <dialkins@tutamail.com>
2025-12-04 14:50:37 +0100

    Done  1. Reading from Redis and recovering original type.

--- 0x0B_redis_basic/exercise.py
+++ 0x0B_redis_basic/exercise.py
@@ -4,7 +4,7 @@ Exercise Redis Module
 """
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -19,3 +19,36 @@ class Cache:
         id = str(uuid4())
         self._redis.set(id, data)
         return id
+
+    def get(self, key: str, fn: Callable = None):
+        """Retrieve data from redis."""
+        if not key:
+            return None
+
+        res = self._redis.get(key)
+        if not res:
+            return None
+        if not fn:
+            return res
+        else:
+            return fn(res)
+
+    def get_str(self, key: str):
+        """Retrieve string from redis."""
+        if not key:
+            return None
+
+        def fn(x):
+            """Lambda function"""
+            return str(x)
+        self.get(key, fn)
+
+    def get_int(self, key: str):
+        """Retrieve integer from redis."""
+        if not key:
+            return None
+
+        def fn(x):
+            """Lambda function"""
+            return int(x)
+        self.get(key, fn)
