b092981d8933de986dda8c16f5e70a3c2feaf596
cableyesto <dialkins@tutamail.com>
2025-12-12 10:40:31 +0100

    fix: add previous projects 8-api

--- unittests_in_js/9-api/api.test.js
+++ unittests_in_js/9-api/api.test.js
@@ -5,6 +5,35 @@ describe("Index page", function() {
   let options;
   beforeEach(function () {
     options = {
+      uri: "http://127.0.0.1:7865",
+      method: "GET"
+    };
+  });
+  it("should return status code 200 for `GET /`", function() {
+    request(options, function(error, response, body) {
+      expect(response.statusCode).to.equal(200);
+    });
+  });
+  it("should return correct body for `GET /`", function() {
+    request(options, function(error, response, body) {
+      expect(body).to.equal("Welcome to the payment system");
+    });
+  });
+  it("should return status code 404 for `POST /`", function() {
+    const options_post = {
+      uri: "http://127.0.0.1:7865",
+      method: "POST"
+    };
+    request(options_post, function(error, response, body) {
+      expect(response.statusCode).to.equal(404);
+    });
+  });
+})
+
+describe("Cart page", function() {
+  let options;
+  beforeEach(function () {
+    options = {
       uri: "http://127.0.0.1:7865/cart/",
       method: "GET"
     };
