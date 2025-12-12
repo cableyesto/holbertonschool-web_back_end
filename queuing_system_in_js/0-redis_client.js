bcd9276ca2758f9f793fc6059ae8b6de2bd03aa2
cableyesto <dialkins@tutamail.com>
2025-12-12 15:03:50 +0100

    feat: done task 1. Node Redis Client.

---
+++ queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,12 @@
+import { createClient } from "redis";
+
+const client = createClient();
+
+client.on("error", function(err) {
+    console.log(`Redis client not connected to the server: ${err}`);
+    // console.error(`Redis client not connected to the server: ${err}`);
+});
+
+client.on("connect", function() {
+    console.log("Redis client connected to the server");
+});
